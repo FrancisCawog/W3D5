7c284952b68785111879cec4bb76764870a09aeb
Francis Cawog <Franciscawog@gmail.com>
2023-09-11 15:00:16 -0400

    fixed valid moves

--- practice-for-ch-ruby-knights-travails-long-practice-main/knight_path_finder.rb
+++ practice-for-ch-ruby-knights-travails-long-practice-main/knight_path_finder.rb
@@ -3,7 +3,6 @@ require "byebug"
 
 class KnightPathFinder
 
-
     def initialize(start_position)
         @root_node = PolyTreeNode.new(start_position) 
         @considered_positions = [start_position]
@@ -12,12 +11,10 @@ class KnightPathFinder
     def self.valid_moves?(pos)
         moves = [[1, 2], [1, -2], [-1, 2], [-1, -2], [2, 1], [2, -1], [-2, 1], [-2, -1]]
         new_arr = []
-        # debugger
         moves.each do |move|
-            new_arr << [pos.value[0] + move[0], pos.value[1] + move[1]]
+            new_arr << [pos[0] + move[0], pos[1] + move[1]]
         end
-
-        new_arr.select {|pos| pos[0] >= 0 && pos[0] <= 7 && pos[0] >= 0 && pos[0] <= 7}
+        new_arr.select {|pos| (pos[0] >= 0 && pos[0] <= 7) && (pos[1] >= 0 && pos[1] <= 7)}
     end 
 
     def new_move_positions(pos)
@@ -27,17 +24,14 @@ class KnightPathFinder
         selected_pos
     end
 
-    def build_move_tree(end_pos)
-        queue = [@root_node.value]
+    def build_move_tree
+        queue = [@root_node]
         until queue.empty?
             ele = queue.shift
-            # debugger
-            return ele.value if ele.value == end_pos
-            queue += new_move_positions(ele)
+            queue += PolyTreeNode.new(new_move_positions(ele))
         end
         return nil 
     end 
-
 
     def [](pos)
         row, col = pos
@@ -51,16 +45,5 @@ class KnightPathFinder
 end 
 
 
-
 puts a = KnightPathFinder.new([0,0])
 puts a.build_move_tree([3, 3])
-
-
-
-
-
-
-
-
-
-
